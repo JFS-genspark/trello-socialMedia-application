8f050f975130b166cca980472d7ec35874e357ff
Balaramansethu <baalaaraman@gmail.com>
2025-05-06 23:24:17 +0530

    feat - implement display recent post whiach sorts the list in descending order.

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
         Trello trello = new Trello();
         trello.postTweetFunction(101,"Hello it is my first post !!");
         trello.postTweetFunction(102,"Hello it is my second post !!");
-        trello.followFunction(101,22);
+        trello.postTweetFunction(102,"Hello it is my third post !!");
+        trello.postTweetFunction(102,"Hello it is my fourth post !!");
+        trello.postTweetFunction(102,"Hello it is my fifth post !!");
+        trello.followFunction(101,102);
         trello.unfollowFunction(102,22);
         System.out.println(trello.followersMap);
-        trello.displayPost();
+        trello.displayRecentPosts();
     }
 }
--- src/Trello.java
+++ src/Trello.java
@@ -6,11 +6,11 @@ public class Trello
     LinkedList list = new LinkedList();
 
     Map<Integer, Set<Integer>> followersMap = new HashMap<>(); // stores the followers
-    int timecalculation = 1;
+    int timeCalculation = 1;
 
     public void postTweetFunction (int userId, String tweet){ // post  tweet using the userId with timestamp included
         if(Objects.equals(tweet, " ")) return ;
-        list.buildList(userId,tweet,timecalculation++);
+        list.buildList(userId,tweet,timeCalculation++);
     }
 
     public void followFunction (int followerId, int followingId){ // user can follow another user
@@ -25,10 +25,58 @@ public class Trello
         }
     }
 
-    public void displayPost(){
-        while (list.head != null) {
-            System.out.println(  "[userId :"+list.head.data+ ", " + list.head.tweet+", Tweet stampNo : "+ list.head.time +"]");
-            list.head = list.head.next;
+    public void displayRecentPosts() {
+        //  Filter relevant tweets into a new temporary linked list
+        Tweet temp = list.head;
+        Tweet filteredHead = null;
+
+        while (temp != null) {
+            int tweetUserId = temp.data;
+                // Add to filtered list
+                Tweet newNode = new Tweet(tweetUserId, temp.tweet, temp.time);
+                newNode.next = filteredHead;
+                filteredHead = newNode;
+
+            temp = temp.next;
+        }
+
+        // Sort filtered list by time in descending order using bubble sort
+        if (filteredHead == null || filteredHead.next == null) return; // Nothing to sort
+
+        boolean swapped;
+        do {
+            swapped = false;
+            Tweet current = filteredHead;
+            Tweet prev = null;
+
+            while (current.next != null) {
+                if (current.time < current.next.time) {
+                    // Swap nodes
+                    Tweet nextNode = current.next;
+                    current.next = nextNode.next;
+                    nextNode.next = current;
+
+                    if (prev == null) {
+                        filteredHead = nextNode;
+                    } else {
+                        prev.next = nextNode;
+                    }
+
+                    swapped = true;
+                    prev = nextNode;
+                } else {
+                    prev = current;
+                    current = current.next;
+                }
+            }
+        } while (swapped);
+
+        // Print sorted tweets
+        Tweet sorted = filteredHead;
+        while (sorted != null) {
+            System.out.println("[userId: " + sorted.data + ", " + sorted.tweet + ", Tweet stampNo: " + sorted.time + "]");
+            sorted = sorted.next;
         }
     }
+
 }
